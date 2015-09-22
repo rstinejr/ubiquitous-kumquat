5a6bfcf960906727e1d69d34a75f4d7121d5c352
Bob Stine <bstine@lgscout.com>
2015-09-22 17:39:05 -0400

    Add tests for calculate-variance2

--- test/variance/core_test.clj
+++ test/variance/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test  :refer :all]
             [variance.core :refer :all]))
 
-(deftest call-variance
+(deftest call-calculate-variance
   (testing "easy case"
     (let [sample [2 3 4]
           v      (calculate-variance sample)]
@@ -18,4 +18,26 @@
             err   (- exp v)
             pct   (/ err exp)
             pct2  (* pct pct)]
-        (is (> 0.00001 pct2))))))
+        (is (> 0.00001 pct2)))))
+  (testing "no samples"
+    (is (thrown? Exception (calculate-variance [])))))
+
+(deftest call-calculate-variance2
+  (testing "easy case"
+    (let [sample [2 3 4]
+          v      (calculate-variance2 sample)]
+      (let [exp   (double (/ 2.0 3.0))
+            err   (- exp v)
+            pct   (/ err exp)
+            pct2  (* pct pct)]
+        (is (> 0.00001 pct2)))))
+  (testing "bigger variance"
+    (let [sample [0 1 2 3 4 5 6]
+          v      (calculate-variance2 sample)]
+      (let [exp   (double (/ 28.0 7.0))
+            err   (- exp v)
+            pct   (/ err exp)
+            pct2  (* pct pct)]
+        (is (> 0.00001 pct2)))))
+  (testing "no samples"
+    (is (thrown? Exception (calculate-variance2 [])))))

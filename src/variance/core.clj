14dd7fbf6b06270c412f7d3e495e3187409157c2
R.H. Stine <bob@waltonstine.net>
2015-09-22 07:01:38 -0400

    Start roughing out an alternative algorithm that uses map/reduce instead of loop.

--- src/variance/core.clj
+++ src/variance/core.clj
@@ -14,8 +14,18 @@
       (throw (ex-info "Most have one or more samples to compute variance." {:causes #{:div-by-zero :bad-args}})))
     (double (- (/ ss cnt) (/ (/ (* s s) cnt) cnt)))))
 
+(defn calculate-variance2
+  [sample]
+  (let [[ss s] (reduce 
+                     (fn [pair1 pair2] 
+                       [ (+ (pair1 0) (pair2 0)) (+ (pair1 1) (pair2 1))])
+                     [0 0] 
+                     [[4 2 ] [9 3] [1 1] [0 0]])]
+        (println (str "sum sqrs: " ss ", sum: " s ", count: " (count sample)))))
+
 (defn -main
   [& args]
   (let [sample (concat (map #(Integer. %) args))]
     (println "sample:" sample)
+    (calculate-variance2 sample)
     (println (str "variance: " (calculate-variance sample)))))

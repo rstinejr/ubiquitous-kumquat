2b54e6629ae23cbdcf5aa7485fa50911d7b0b26b
R.H. Stine <bob@waltonstine.net>
2015-09-26 19:10:40 -0400

    Common test function for all calc-variance functions for easy case.

--- test/variance/core_test.clj
+++ test/variance/core_test.clj
@@ -8,13 +8,17 @@
 (def big-sample [0 1 2 3 4 5 6])
 (def big-expect (double (/ 28.0 7.0)))
 
+(defn run-easy
+  [f]
+  (let [v    (f easy-sample)
+        err  (- easy-expect v)
+        pct  (/ err easy-expect)
+        pct2 (* pct pct)]
+    (> 0.00001 pct2)))
+
 (deftest test-calculate-variance1
   (testing "easy case"
-    (let [v      (calculate-variance1 easy-sample)]
-      (let [err   (- easy-expect v)
-            pct   (/ err easy-expect)
-            pct2  (* pct pct)]
-        (is (> 0.00001 pct2)))))
+    (is (run-easy calculate-variance1)))
   (testing "bigger variance"
     (let [v      (calculate-variance1 big-sample)]
       (let [err   (- big-expect v)
@@ -26,11 +30,7 @@
 
 (deftest test-calculate-variance2
   (testing "easy case"
-    (let [v      (calculate-variance2 easy-sample)]
-      (let [err   (- easy-expect v)
-            pct   (/ err easy-expect)
-            pct2  (* pct pct)]
-        (is (> 0.00001 pct2)))))
+    (is (run-easy calculate-variance2)))
   (testing "bigger variance"
     (let [v      (calculate-variance2 big-sample)]
       (let [err   (- big-expect v)
